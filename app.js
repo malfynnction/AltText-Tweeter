d920c3a28519c9a63f9e71983c568dcfa6da10b4
Fynn <fynn.heintz@gmail.com>
2018-03-08 15:42:34 +0100

    fixed a bug concerning image description with more than 280 chars

--- app.js
+++ app.js
@@ -9,27 +9,29 @@ Twitter.stream('statuses/filter', {track: '@get_altText'}, function(stream) {
     var m_us = tweet.user.screen_name;
     var o_id = tweet.in_reply_to_status_id_str;
 
-    if (o_id != null) {
-      Twitter.get('statuses/show', {id: o_id, include_ext_alt_text: 'true', tweet_mode: 'extended'}, function(err, o_tweet){
-        if(err) {
-          console.log(err);
-        }
-
-        if (o_tweet.extended_entities == undefined || o_tweet.extended_entities['media'] == undefined) {
-          tweetThis('You don\'t seem to be replying to a tweet containing an image. I might be wrong, this is all very confusing.', m_id, m_us);
-        }
-
-        else {
-          var media = o_tweet.extended_entities['media'];
-          var cont = '';
-          for (var i = 0; i < media.length; i++) {
-            if (media.length > 1) cont += (i+1) + '. Pic: ' + media[i].ext_alt_text + '\n';
-            else cont += media[i].ext_alt_text;
-          }
-          tweetThis(cont, m_id, m_us);
-        }
-      })
+    if (o_id == null) {
+      o_id = m_id;
     }
+
+    Twitter.get('statuses/show', {id: o_id, include_ext_alt_text: 'true', tweet_mode: 'extended'}, function(err, o_tweet){
+      if(err) {
+        console.log(err);
+      }
+
+      if (o_tweet.extended_entities == undefined || o_tweet.extended_entities['media'] == undefined) {
+        tweetThis('You don\'t seem to be replying to a tweet containing an image.', m_id, m_us);
+      }
+
+      else {
+        var media = o_tweet.extended_entities['media'];
+        var cont = '';
+        for (var i = 0; i < media.length; i++) {
+          if (media.length > 1) cont += (i+1) + '. Pic: ' + media[i].ext_alt_text + '\n';
+          else cont += media[i].ext_alt_text;
+        }
+        tweetThis(cont, m_id, m_us);
+      }
+    })
   });
 
   stream.on('err', function(err) {
@@ -38,7 +40,7 @@ Twitter.stream('statuses/filter', {track: '@get_altText'}, function(stream) {
 });
 
 function tweetThis (content, to_id, to_us) {
-  content = content.replace('null', 'There is no alt text for this image, i\'m sorry.');
+  content = content.replace(/null/g, 'There is no alt text for this image, i\'m sorry.');
   
   var content = '@' + to_us + ' ' + content;
 
@@ -53,16 +55,14 @@ function tweetThis (content, to_id, to_us) {
 
   else {
     var cont_arr = content.split(' ');
-    cont_arr.splice(0,1);
     var part = '';
 
-    while(part.length < 260){
-      if (cont_arr.length == 0) break;
+    while(cont_arr.length > 0 && (part.length + cont_arr[0].length) <= 280){
       part += cont_arr[0] + ' ';
       cont_arr.splice(0,1);
     }
 
-    var reply = {status: '@' + to_us + ' ' + part, in_reply_to_status_id: to_id}
+    var reply = {status: part, in_reply_to_status_id: to_id}
     Twitter.post('statuses/update', reply,  function(err, tweet){
       if(err){
         console.log(err);
