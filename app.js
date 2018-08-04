e7f43fe4fcaa46fe5aa07e21eb2f139abbdff17f
Fynn <fynn.heintz@gmail.com>
2018-08-04 22:42:54 +0200

    now also looking at quoted tweets if theres no pic in the original one

--- app.js
+++ app.js
@@ -21,27 +21,22 @@ Twitter.stream('statuses/filter', {track: '@get_altText'}, function(stream) {
       //ppl posting the pic and triggering the bot within the same tweet
       if (o_id == null) {
         o_id = m_id;
+        o_us = m_us;
       }
 
+      //get the tweet with the picture
       Twitter.get('statuses/show', {id: o_id, include_ext_alt_text: 'true', tweet_mode: 'extended'}, function(err, o_tweet){
         if(err) {
           console.log(err);
         }
 
-        if (o_tweet.extended_entities == undefined || o_tweet.extended_entities['media'] == undefined) {
-          tweetThis('You don\'t seem to be replying to a tweet containing an image.', m_id, m_us, o_us);
-        }
+        var content = read(o_tweet, m_id, m_us, o_us);
 
-        else {
-          var media = o_tweet.extended_entities['media'];
-          var cont = '';
-          for (var i = 0; i < media.length; i++) {
-            if (media.length > 1) cont += (i+1) + '. Pic: ' + media[i].ext_alt_text + '\n';
-            else cont += media[i].ext_alt_text;
-          }
-          tweetThis(cont, m_id, m_us, o_us);
+        //check just to make sure that you're not stuck in that callback shit
+        if (content !== '') {
+          tweetThis(content, m_id, m_us, o_us);
         }
-      })
+      });
     }
   });
 
@@ -49,6 +44,38 @@ Twitter.stream('statuses/filter', {track: '@get_altText'}, function(stream) {
     console.log(err);
   });
 });
+
+function read (tw, m_id, m_us, o_us) {
+  var alt = '';
+
+  //no media in this tweet
+  if (tw.extended_entities == undefined || tw.extended_entities['media'] == undefined) {
+    if (!tw.is_quote_status) {
+      alt += 'I can\'t find any image in that tweet, sorry.';
+    }
+    //maybe in the quoted tweet?
+    else {
+      Twitter.get('statuses/show', {id: tw.quoted_status_id_str, include_ext_alt_text: 'true', tweet_mode: 'extended'}, function(err, q_tweet){
+        if (err) {
+          console.log(err);
+        }
+
+        tweetThis (read(q_tweet, m_id, m_us, q_tweet.user.screen_name), m_id, m_us, q_tweet.user.screen_name);
+      });
+    }
+  }
+
+  else {
+    var media = tw.extended_entities['media'];
+    for (var i = 0; i < media.length; i++) {
+      if (media.length > 1) alt += (i+1) + '. Pic: ' + media[i].ext_alt_text + '\n';
+      else alt += media[i].ext_alt_text;
+    }
+  }
+
+  return alt;
+}
+
 
 function tweetThis (content, to_id, to_us, o_us) {
   //pls no loops of death!
@@ -58,7 +85,7 @@ function tweetThis (content, to_id, to_us, o_us) {
 
   content = content.replace(/null/g, 'There is no alt text for this image, i\'m sorry. @' + o_us + ' it\'d be cool if you added descriptions to your images in the future, so that users with a screenreader know what\'s shown, too! Here\'s how you can do that: https://help.twitter.com/en/using-twitter/picture-descriptions');
   
-  var content = '@' + to_us + ' ' + content;
+  content = '@' + to_us + ' ' + content;
 
   if (content.length <= 280) {
     var reply = {status: content, in_reply_to_status_id: to_id}
