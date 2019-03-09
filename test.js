87eed6a2fd7a29ede81270622ecd62b290e7e443
Fynn Heintz <fynn.heintz@gmail.com>
2019-03-09 20:25:02 +0100

    write unit tests

---
+++ test.js
@@ -0,0 +1,69 @@
+const test = require('ava')
+const testTweets = require('./test-tweets.json')
+const getReplyText = require('./get-reply-text')
+
+const noAltText = "There is no alt text for this image, i'm sorry."
+const pleaseDoInTheFuture = "it'd be cool if you added descriptions to your images in the future to improve the accessibility! Here's how you can do that: https://help.twitter.com/en/using-twitter/picture-descriptions"
+const noImage = "I can't find any images in the tweet you're replying to or in your own tweet, sorry."
+const noImageInQuote = "I can't find any images in the tweet you're replying to, nor in your own tweet or the tweet you quoted, sorry."
+const video = "This is a video. Unfortunately, twitter doesn't allow to add alt texts for videos yet. \n @twitter, it'd be cool if you changed that!"
+
+test('classic flow, with alt text', async t => {
+  const reply = getReplyText(testTweets[1])
+  t.is(await reply, "@SnoringDoggo Athena the fluffy white and brown cat looks up innocently in front of a plush")
+})
+
+test('classic flow, no alt text', async t => {
+  const reply = getReplyText(testTweets[2])
+  t.is(await reply, `@solitaryrainbow ${noAltText} @IAStartingLine ${pleaseDoInTheFuture}`)
+})
+
+test('triggering = original, no alt text', async t => {
+  const reply = getReplyText(testTweets[3])
+  t.is(await reply, `@womensart1 ${noAltText} @womensart1 ${pleaseDoInTheFuture}`)
+})
+
+test('triggering = original, with alt text', async t => {
+  const reply = getReplyText(testTweets[4])
+  t.is(await reply, "@foxeen Winzige runde Tropfen entlang den fast unsichtbaren Fäden eines Spinnennetzes in Nahaufnahme - manche sind ganz scharf, manche unscharf, aber alle zusammen wirken wie feinste Perlen-Spitze, meisterlich geklöppelt.")
+})
+
+test('image in quote, alt text', async t => {
+  const reply = getReplyText(testTweets[5])
+  t.is(await reply, "@SehrysFlausch a tabby cat (@skyescats #OhMyOllie) sits on a chair, with its head looking up over a kitchen table. on the table is an unfinished jigsaw puzzle, the picture also featuring two tabby cats.")
+})
+
+test('image in quote, no alt text', async t => {
+  const reply = getReplyText(testTweets[6])
+  t.is(await reply, `@_cumasyouare_ ${noAltText} @blurryechopilot ${pleaseDoInTheFuture}`)
+})
+
+test('no quote, no reply, no image', async t => {
+  const reply = getReplyText(testTweets[7])
+  t.is(await reply, `@malfynnction ${noImage}`)
+})
+
+test('reply, but no image anywhere', async t => {
+  const reply = getReplyText(testTweets[8])
+  t.is(await reply, `@malfynnction ${noImage}`)
+})
+
+test('quote, but no image anywhere', async t => {
+  const reply = getReplyText(testTweets[9])
+  t.is(await reply, `@malfynnction ${noImageInQuote}`)
+})
+
+test('image in triggering as well as original', async t => {
+  const reply = getReplyText(testTweets[10])
+  t.is(await reply, "@malfynnction My new ID, stating that my first name is Fynn and my second name is Julius, everything else - except the photo - is blacked out")
+})
+
+test('tweet by bot itself', async t => {
+  const reply = getReplyText(testTweets[11])
+  t.is(await reply, undefined)
+})
+
+test('video', async t => {
+  const reply = getReplyText(testTweets[12])
+  t.is(await reply, `@souplemur ${video}`)
+})
