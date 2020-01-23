b9da6ff3411bc09807193879fe9cb99dd0645e5c
Fynn Heintz <fynn.heintz@gmail.com>
2020-01-23 21:30:47 +0100

    directly return promise

--- get-tweet.js
+++ get-tweet.js
@@ -4,19 +4,11 @@ const keys = require('./keys')
 const Twitter = new TwitterP(keys)
 
 module.exports = async id => {
-  return new Promise((resolve, reject) => {
-    Twitter.get(
+  return Twitter.get(
     'statuses/show',
     {
       id: id,
       include_ext_alt_text: 'true',
       tweet_mode: 'extended'
     })
-    .then(tweet => {
-      resolve(tweet)
-    })
-    .catch( error => {
-      reject(error)
-    })
-  })
 }
