afab16856ac5989458f85352dbf2357efffeee90
Fynn Heintz <fynn.heintz@gmail.com>
2020-10-10 21:16:08 +0200

    don't try to tweet more than 280 characters

--- send-tweet.js
+++ send-tweet.js
@@ -6,7 +6,7 @@ const Twitter = new TwitterP(keys)
 const tweetThis = (content, toriginal_id, mentioning_user) => {
   if (content.length <= 280) {
     const reply = { status: content, in_reply_to_status_id: toriginal_id }
-    Twitter.post('statuses/update', reply, function(err, tweet) {
+    Twitter.post('statuses/update', reply, function (err, tweet) {
       if (err) {
         console.log(err)
       }
@@ -15,17 +15,21 @@ const tweetThis = (content, toriginal_id, mentioning_user) => {
     const cont_arr = content.split(' ')
     let part = ''
 
-    while (cont_arr.length > 0 && part.length + cont_arr[0].length <= 280) {
+    while (cont_arr.length > 0 && part.length + cont_arr[0].length <= 279) {
       part += cont_arr[0] + ' '
       cont_arr.splice(0, 1)
     }
 
     const reply = { status: part, in_reply_to_status_id: toriginal_id }
-    Twitter.post('statuses/update', reply, function(err, tweet) {
+    Twitter.post('statuses/update', reply, function (err, tweet) {
       if (err) {
         console.log(err)
       }
-      tweetThis(`@${mentioning_user} ${cont_arr.join(' ')}`, tweet.id_str, mentioning_user)
+      tweetThis(
+        `@${mentioning_user} ${cont_arr.join(' ')}`,
+        tweet.id_str,
+        mentioning_user
+      )
     })
   }
 }
