83f2e6390aba4abafa8c7f40d81079634627f556
Fynn Heintz <fynn.heintz@gmail.com>
2021-01-20 17:12:20 +0100

    fix: the tweet returned by the stream is sometimes broken
    when it was in reply to a tweet by the bot itself

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const TwitterP = require('twitter')
 const keys = require('./keys')
 const sendTweet = require('./send-tweet')
 const getReplyText = require('./get-reply-text')
+const getTweet = require('./get-tweet')
 
 const Twitter = new TwitterP(keys)
 
 Twitter.stream('statuses/filter', { track: '@get_altText' }, function (stream) {
-  stream.on('data', (tweet) => {
-    const mentioning_id = tweet.id_str
-    const mentioning_user = tweet.user.screen_name
+  stream.on('data', (broken_tweet) => {
+    const mentioning_id = broken_tweet.id_str
+    const mentioning_user = broken_tweet.user.screen_name
 
-    getReplyText(tweet)
-      .then((reply) => {
-        if (reply && reply.length > 0 && reply !== 'undefined') {
-          sendTweet(reply, mentioning_id, mentioning_user)
-        }
-      })
-      .catch((err) => console.error(err))
-  })
+    getTweet(mentioning_id).then((tweet) => {
+      getReplyText(tweet)
+        .then((reply) => {
+          if (reply && reply.length > 0) {
+            sendTweet(reply, mentioning_id, mentioning_user)
+          }
+        })
+        .catch((err) => console.error(err))
+    })
 
-  stream.on('error', function (err) {
-    console.log(err)
+    stream.on('error', function (err) {
+      console.log(err)
+    })
   })
 })
